682acc3465ab53545adacb79bd2584df0d90606b
Pietro Torri <ptrotrri@gmail.com>
2021-01-28 18:52:27 +0100

    epic changes

---
+++ Thelastdarkness/Assets/Scripts/Movement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{ 
+    public CharacterController controller;
+    public Transform cam;
+
+    //Walking
+    public float speed;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //for Walk
+        Walk();
+    }
+
+    private void Walk()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 dir = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (dir.magnitude * speed >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
