23651f2b52f4cb8e20d6df84fbc1f3c8ec47f0d9
Pietro Torri <ptrotrri@gmail.com>
2021-02-06 22:00:05 +0100

    ch

---
+++ The Last Darkness/Assets/Codes/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float maxHealth, maxThirst, maxHunger;
+    public float thirstIncreaseRate, hungerIncreaseRate;
+    public float health, thirst, hunger;
+
+    public bool dead;
+
+    public void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void Update()
+    {
+        if (!dead)
+        {
+            hunger += hungerIncreaseRate * Time.deltaTime;
+            thirst += thirstIncreaseRate * Time.deltaTime;
+        }
+
+        if (thirst >= maxThirst)
+            Die();
+        if (hunger >= maxHunger)
+            Die();
+            
+    }
+
+    public void Die()
+    {
+        dead = true;
+        print("YOU DIED");
+    }
+}
