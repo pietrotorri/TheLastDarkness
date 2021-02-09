9da3ff538f78df9ce73b905f49ce1bcb969accee
Pietro Torri <ptrotrri@gmail.com>
2021-02-09 18:18:48 +0100

    bih

---
+++ The Last Darkness/Assets/Codes/Slot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private bool hovered;
+    private bool used;
+
+    private GameObject item;
+    private Texture itemIcon;
+
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        hovered = true;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        hovered = false;
+    }
+}
