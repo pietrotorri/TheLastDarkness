ecd5ab429def8b42c55aa9a3bf27bfed195eaaf1
Pietro Torri <ptrtorri@gmail.com>
2021-02-09 20:10:48 +0100

    lelllo

--- The Last Darkness/Assets/Codes/Inventory.cs
+++ The Last Darkness/Assets/Codes/Inventory.cs
@@ -4,12 +4,41 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-
     public GameObject inventory;
     public GameObject slotHolder;
 
     private int slots;
-    private GameObject[] slot;
+    private Transform[] slot;
 
+    public void Start()
+    {
+        // slots being detected
+        slots = slotHolder.transform.childCount;
+        slot = new Transform[slots];
+        DetectInventorySlots();
+    }
 
+    public void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    public void AddItem(GameObject item)
+    {
+
+    }
+
+    public void DetectInventorySlots()
+    {
+        for(int i = 0; i < slots; i++)
+        {
+            slot[i] = slotHolder.transform.GetChild(i);
+            print(slot[i].name);
+        }
+    }
 }
